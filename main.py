b5e7478b6bed495bfeb060cb03e1ad00f8995154
Lucas Cantoni <lukcnt@outlook.com>
2022-08-04 07:03:21 -0300

    Added the paths of the game.

--- main.py
+++ main.py
@@ -30,3 +30,26 @@ print('''
 # Greetings and objective of the game.
 print("Welcome to Hogwarts School of Witchcraft and Wizardry.")
 print("Your mission is to reach in Gryffindor Tower.")
+
+# The game.
+choice1 = input('You\'re at the Great Hall. Where do you want to go? Type "stairs" or "corridor": ')
+if choice1.lower() == "stairs":
+    choice2 = input('You were walking up the stairs when they start moving. What you gonna do? Type "jump" or "stay": ')
+    if choice2.lower() == "jump":
+        choice3 = input('You found it!!!!! Now you need the password to access the common room. Choose one of three "Fortuna Major", "Abracadabra" or "Wingardium Leviosa": ')
+        if choice3.lower() == "fortuna major":
+            print("You did it, got it right. Now have full access to the common room and dormitories of Gryffindor. You WIN!!!!")
+        elif choice3.lower() == "abracadabra":
+            print("The guardian got scared and thought that you were trying to kill him, you got suspended. Game Over")
+        elif choice3.lower() == "wingardium leviosa":
+            print("You got yelled at for using a speell on the guardian. Game Over")
+        else:
+            print("That's not a option. Game Over")
+    elif choice2.lower() == "stay":
+        print("You ended up in a dungeon, got attacked by a troll and died... Game Over")
+    else:
+        print("That's not a option. Game Over")
+elif choice1.lower() == "corridor":
+    print("You got lost in the castle. Game Over")
+else:
+    print("That's not a option. Game Over")
